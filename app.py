90b991a60fd08f3c3c7f9a7416c496bd63ac53d1
F1nnC <captainsurfervideos@gmail.com>
2024-11-08 14:04:24 -0800

    ok all templates are done now

--- app.py
+++ app.py
@@ -20,5 +20,10 @@ def login():
 
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port="8012")
